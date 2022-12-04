46789e7ed46a03efeeff87744c83e7e993cd924b
shubham <shubhamsarkar2587@gmail.com>
2022-12-05 03:25:37 +0530

    Title: fixed payment controller Dev: Shubham Date: 5 Dec 2022

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,16 +1,18 @@
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 
 const createPayment = async (req, res) => {
+  console.log(process.env.STRIPE_KEY);
   try {
-    stripe.charges.create({
-      source: req.body.tokenId,
+    stripe.paymentIntents.create({
+      // source: req.body.tokenId,
       amount: req.body.amount,
-      currency: "usd"
-    }, (err, res) => {
-      if (err) {
-        res.status(500).json({ errMsg: err });
+      currency: "usd",
+      payment_method_types: ['card'],
+    }, (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json({ errMsg: stripeErr });
       } else {
-        res.status(200).json(res)
+        res.status(200).json(stripeRes);
       }
     })
   } catch (err) {
