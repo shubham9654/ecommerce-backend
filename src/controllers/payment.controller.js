91d55a94152f5c4da92adec37bac9f6124e3a31d
shubham <shubhamsarkar2587@gmail.com>
2022-12-05 07:11:22 +0530

    Title: fixed stripe payment controller Dev: Shubham Date: 5 Dec 2022

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,20 +1,33 @@
+const { extractNum } = require('../utils/global.helper');
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 
 const createPayment = async (req, res) => {
-  console.log(process.env.STRIPE_KEY);
   try {
-    stripe.paymentIntents.create({
-      // source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-      payment_method_types: ['card'],
-    }, (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json({ errMsg: stripeErr });
-      } else {
-        res.status(200).json(stripeRes);
-      }
+    const line_items = req.body.cartItems.map(item => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.title,
+          images: [item.img],
+          description: item.desc,
+          metadata: {
+            id: item._id,
+            size: item.size,
+            color: item.color,
+          }
+        },
+        unit_amount: extractNum(item.price) * 100
+      },
+      quantity: item?.quantity || 1,
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/checkout-success`,
+      cancel_url: `${process.env.CLIENT_URL}/cart`
     })
+    res.status(200).send({ url: session.url })
   } catch (err) {
     res.status(500).json({ errMsg: err });
   }
---
+++ src/utils/global.helper.js
@@ -0,0 +1,7 @@
+const extractNum = (str) => {
+  return str.replace(/[^0-9]/g,'');
+}
+
+module.exports ={
+  extractNum
+}
